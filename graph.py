9f9c6c34387688d8c33b27f2643c8b844daf8a25
Conrad Sadler <141091490+conradSadler@users.noreply.github.com>
2025-12-01 09:33:21 -0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -9,17 +9,15 @@ x, y, z = np.meshgrid(np.arange(-4, 4, 1),
                       np.arange(-4, 4, 1),
                       np.arange(0, 4, 1))
 
-ax.quiver(x, y, z, -y, x, -z, length=0.5, normalize=True)
-ax.set_title("The Vector Feild F = [-y,x,-z]")
+ax.quiver(x, y, z, -y-x, x-y, -z, length=0.6, normalize=True)
+ax.set_title("The Vector Feild F = [-y-x,x-y,-z]")
 ax.axes.get_xaxis().set_ticks([])
 ax.axes.get_yaxis().set_ticks([])
 ax.axes.get_zaxis().set_ticks([])
 
 def animate(i):
-    ax.view_init(elev=0, azim=(i*0.1))
+    ax.view_init(elev=30, azim=(i*0.1))
     return ax,
 
 ani = FuncAnimation(fig, animate,frames=3600, interval=20, blit=False)
 print(ani.to_html5_video())
-
-#idea, have a top view of a stream going down the drain and have a side rotational view. make then side by side  and have them autoplay
